5fddc70e8e5de5b750d6009cf2675fdbb01b99d4
Brandon Ramos <artbyramos@gmail.com>
2023-03-01 20:28:36 -0500

    JavaScript Fundamentals - Part 1 to Part 1.2

---
+++ javascript-fundamentals_part-01.js
@@ -0,0 +1,82 @@
+// ====== Javascript Fundamentals - Part 1 ======
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = 'Matilda';
+let first = 'Jonas';
+let firstNamePerson; // camelCase naming convention(standard for JavaScript)
+let first_name_person; // Ruby naming convention using underscores
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//let 3years = 3; Can't name variables with a numerical value
+
+let jonas_matilda = 'JM';
+let $jonas_matilda = 'JM'; // variable names can start with the money sign as well
+// let function = 27; reserved variable name and will throw an error
+
+let PI = 3.1435; // if it's a real constant, write it in uppercase like this
+
+let myFirstJob = 'Programmer'; // make variable names descriptive
+let myCurrentJob = 'Teacher'; // make variable names descriptive
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+
+// ASSIGNMENT #1 - Values and Variables
+let country = 'United States';
+let continent = 'North America';
+let population = 332.28 + " million";
+console.log(country, continent, population);
+
+
+
+// ====== JavaScript Fundamentals - Part 1.2 ======
+// 7 primitive data types
+//  1. Number: Floating point numbers - used for decimals and integers. ex: let age = 23;
+//  2. String: Sequence of characters - used for text. ex: let firstName = 'jonas';
+//  3. Boolean: Logical type that can only be 'true' or 'false' - used for taking decisions. ex: let fullAge = true;
+//  4. Undefined: Value taken by a variable that is not yet defined ("empty value"). ex: let children;
+//  5. Null: Also means 'empty value'.
+//  6. Symbol (ES2015): Value that is unique and cannot be changed.
+//  7. BigInt (ES2020): Larger integers than the Number type can hold.
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); // typeof checks the value data type
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+// ASSIGNMENT #2 - Data Types
+let isIsland = 'Hawaii';
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+
+// ====== JavaScript Fundamentals - Part 1.3 ======
